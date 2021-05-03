101806635318fb06a1e6d76177214c5451c9b094
watsjn <aliaksei.shumovich@gmail.com>
2021-05-03 20:08:45 +0200

    - missed type added

--- api/public/index.php
+++ api/public/index.php
@@ -12,6 +12,5 @@ require __DIR__ . '/../vendor/autoload.php';
 /** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-/** @var App $app */
 $app = (require __DIR__ . '/../config/app.php')($container);
 $app->run();
