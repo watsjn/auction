ba665f422c02c08c4ce84e9674070ecc419311bf
watsjn <aliaksei.shumovich@gmail.com>
2021-05-03 21:15:22 +0200

    - disabled logging errors while tests

--- api/tests/Functional/HomeTest.php
+++ api/tests/Functional/HomeTest.php
@@ -11,6 +11,13 @@ use App\Http\Action\HomeAction;
  */
 class HomeTest extends WebTestCase
 {
+    public function testMethod(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/'));
+
+        self::assertEquals(405, $response->getStatusCode());
+    }
+
     public function testSuccess(): void
     {
         $response = $this->app()->handle(self::json('GET', '/'));
