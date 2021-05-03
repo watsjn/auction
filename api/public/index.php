a799c3c3ae107daa678dde383f2677654b0bdc8f
watsjn <aliaksei.shumovich@gmail.com>
2021-05-03 15:13:59 +0200

    - extracted action into class

--- api/public/index.php
+++ api/public/index.php
@@ -2,21 +2,27 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Http;
 use Slim\Factory\AppFactory;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
 
-$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+]);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('{}');
-    return $response->withHeader('Content-Type', 'application/json');
-});
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
+
+$app->get('/', Http\Action\HomeAction::class);
 
 $app->run();
